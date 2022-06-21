f8c61d53d2ad3b4720ba5bacb7028ad5c29d9133
rezaahmadn <rezaahmadn@gmail.com>
2022-06-21 17:36:29 +0700

    adding imageUrl column to category

---
+++ server/migrations/20220621102143-add-imageUrl-column.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn("Categories", "imageUrl", Sequelize.STRING)
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn("Categories", "imageUrl", {})
+  }
+};
